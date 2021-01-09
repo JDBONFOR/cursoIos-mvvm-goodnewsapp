033f76790e030147f24bf2c4d0b473fa80bdfe94
Juan Bonforti <juan@synapticlinks.com.ar>
2021-01-09 18:51:17 -0300

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Juan Bonforti on 09/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
